33c42b716970c9752da7add9ddc1012e5bf6c59c
JEON-HYERIN <jeonhyelin19951118@gmail.com>
2022-08-30 00:55:26 +0900

    0830

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -5,26 +5,76 @@ $(function(){
     });
 
     // header
+    // if($('.btn-nav').hasClass('on')){
+    //   return;
+    // } else {
+    //   $('#gnb .gnb-item').each(function(){
+    //     const length = $('#gnb .gnb-item').length;
+    //     const index = $(this).index();
+    //     for(i=1;i<=$('#gnb .gnb-item').length;i++) {
+    //       $(this).index().css({"transform":"translateY((-17*2) + 'vh')"});
+    //     }
+    //   });
+    // }
+
+    const body = document.querySelector('body');
+    let scrollPosition = 0;
     $('.btn-nav').click(function(){
       $('#gnb').toggleClass('open');
-      
-      if($('#gnb').hasClass('open')) {
-
-        const tl = gsap.timeline({});
+      $('.btn-nav').toggleClass('on');
+      if($('.btn-nav').hasClass('on')) {
+        enable();
+        const tl = gsap.timeline({
+          defaults: {
+            duration: 1.5
+          }
+        });
         tl.addLabel('t1')
-        .to('#gnb',{y: '20vh', scale: 1, transformStyle: 'preserve-3d'}, 't1')
-        .to('#gnb .link-gnb',{y: 0, scale: 1, transformStyle: 'preserve-3d',stagger: .2}, 't1+=.2')
+        .to('.skew2', {transform: 'matrix(-0.7071067690849304,0.7071067690849304,-0.7071067690849304,-0.7071067690849304,489.8043518066406,438.3043518066406)', duration: .3}, 't1')
+        .to('.skew1', {transform: 'matrix(-0.7070367932319641,-0.7071767449378967,0.7071767449378967,-0.7070367932319641,489.81671142578125,189.7080078125)', duration: .3}, 't1')
+        .to('#gnb',{y:'20vh'}, 't1+=.2')
+        .fromTo('#gnb .gnb-item.item1',{y: 0},{y:0}, 't1+=.2')
+        .fromTo('#gnb .gnb-item.item2',{y: '-33.3333vh'},{y:0}, 't1+=.2')
+        .fromTo('#gnb .gnb-item.item3',{y: '-50vh'},{y:0}, 't1+=.2')
+        .fromTo('#gnb .gnb-item.item4',{y: '-66.6667vh'},{y:0}, 't1+=.2')
+        .fromTo('#gnb .gnb-item.item5',{y: '-83.3333vh'},{y:0}, 't1+=.2')
+        .fromTo('#gnb .gnb-item.item6',{y: '-100vh'},{y:0}, 't1+=.2')
       } else {
-          //   gsap.to('#gnb .link-gnb',{
-          //   y: '-33.3333vh',
-          //   stagger: .2
-          // });
-          const tl = gsap.timeline({});
-          tl.addLabel('t1')
-          // .to('#gnb .link-gnb',{y: '-33.3333vh',stagger: .2}, 't1')
-          .to('#gnb',{y: '-20vh'}, 't1')
+        disable();
+        const tl = gsap.timeline({
+          defaults: {
+            duration: 1.5
+          }
+        });
+        tl.addLabel('t1')
+        .to('.skew2', {transform: 'matrix(1,0,0,1,365,315)', duration: .3}, 't1')
+        .to('.skew1', {transform: 'matrix(1,0,0,1,365,315)', duration: .3}, 't1')
+        .fromTo('#gnb .gnb-item.item6',{y: 0},{y: '-100vh' }, 't1+=.2')
+        .fromTo('#gnb .gnb-item.item5',{y: 0},{y: '-83.3333vh'}, 't1+=.2')
+        .fromTo('#gnb .gnb-item.item4',{y: 0},{y: '-66.6667vh'}, 't1+=.2')
+        .fromTo('#gnb .gnb-item.item3',{y: 0},{y: '-50vh'}, 't1+=.2')
+        .fromTo('#gnb .gnb-item.item2',{y: 0},{y: '-33.3333vh'}, 't1+=.2')
+        .fromTo('#gnb .gnb-item.item1',{y: 0},{y: 0}, 't1+=.2')
+        .to('#gnb',{y:'-20vh'}, 't1+=.2')
         }
       });
+
+    // nav open
+    function enable() {
+      scrollPosition = window.pageYOffset;
+      body.style.overflow = 'hidden';
+      body.style.position = 'fixed';
+      body.style.top = `-${scrollPosition}px`;
+      body.style.width = '100%';
+    }
+    // nav close
+    function disable() {
+      body.style.removeProperty('overflow');
+      body.style.removeProperty('position');
+      body.style.removeProperty('top');
+      body.style.removeProperty('width');
+      window.scrollTo(0, scrollPosition);
+    }
 
     // visual
     const visualTl1 = gsap.timeline({
@@ -63,7 +113,7 @@ $(function(){
       });
     });
 
-    $('.sc-gallery .gallery img').each(function(index, element) {
+    $('.sc-gallery .gallery').each(function(index, element) {
       const anim = gsap.to(element, {
         scrollTrigger: {
           trigger: element,
@@ -76,7 +126,7 @@ $(function(){
         delay: (index + 1) * .2,
       });
     });
-    $('.sc-gallery .gallery.reverse img').each(function(index, element) {
+    $('.sc-gallery .gallery.reverse').each(function(index, element) {
       const anim = gsap.to(element, {
         scrollTrigger: {
           trigger: element,
@@ -89,7 +139,7 @@ $(function(){
         delay: (index + 1) * .2,
       });
     });
-    $('.sc-gallery .obj-circle img').each(function(index, element) {
+    $('.sc-gallery .obj-circle').each(function(index, element) {
       const anim = gsap.to(element, {
         scrollTrigger: {
           trigger: element,
@@ -142,7 +192,7 @@ $(function(){
     })
     titleArea1.addLabel('t1')
     .fromTo('.sc-plan .sc-title span',{yPercent: -100},{yPercent: 0},'t1')
-    .fromTo('.sc-plan .sc-sub-title span',{opacity:0, yPercent: 100},{opacity:1, yPercent: 0, stagger: .2},'t1+=.4')
+    .fromTo('.sc-plan .sc-sub-title .txt-wrap span',{opacity:0, yPercent: 100},{opacity:1, yPercent: 0, stagger: .2},'t1+=.4')
     
     const planListTl = gsap.timeline({
       defaults: {
@@ -160,4 +210,8 @@ $(function(){
     .fromTo('.sc-plan .plan-item .num span',{yPercent: -120, opacity: 0},{yPercent: 0,opacity: 1},'t1')
     .fromTo('.sc-plan .plan-item .title strong',{yPercent: 100, opacity: 0},{yPercent: 0,opacity: 1},'t1')
     .fromTo('.sc-plan .plan-item .desc span',{yPercent: 100, opacity: 0},{yPercent: 0,opacity: 1,stagger: 0.05},'t1')
+
+    // year
+    const year = new Date().getFullYear();
+    $('.copyright .year').text(year);
 });
